728303b30d3b5d30d1e3e0e3cfa5ab68e4c5040a
nhamidn <hamid-savana@hotmail.fr>
2023-12-08 00:33:05 +0100

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lockboxes module."""
+
+
+def dfs(boxes, idx, visited):
+    """Function that implement the dfs algorithm."""
+    for key in boxes[idx]:
+        if not visited[key]:
+            visited[key] = True
+            dfs(boxes, key, visited)
+
+
+def canUnlockAll(boxes):
+    """Function that check if all boxes can be opened."""
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+
+    dfs(boxes, 0, visited)
+
+    return all(visited)
