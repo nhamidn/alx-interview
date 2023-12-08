c6033776c1ac1e1a8ed16d5d5abd65e98b1b7e79
nhamidn <hamid-savana@hotmail.fr>
2023-12-08 01:11:08 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,23 +3,18 @@
 
 
 def canUnlockAll(boxes):
-    """Function that check if all boxes can be opened."""
-    if (type(boxes)) is not list:
-        return False
     n = len(boxes)
     if n == 0:
         return False
     visited = [False] * n
     visited[0] = True
 
-    def dfs(boxes, idx, visited):
-        """Function that implement the dfs algorithm."""
-        for key in boxes[idx]:
+    stack = [0]
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
             if not visited[key]:
                 visited[key] = True
-                if not all(visited):
-                    dfs(boxes, key, visited)
-
-    dfs(boxes, 0, visited)
+                stack.append(key)
 
     return all(visited)
