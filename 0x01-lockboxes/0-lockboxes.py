431040681227403c348d03d1b18d8f461ac6d61d
nhamidn <hamid-savana@hotmail.fr>
2023-12-08 02:34:43 +0100

    edge case

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,7 +15,7 @@ def canUnlockAll(boxes):
     while queue:
         current_box = queue.pop(0)
         for key in boxes[current_box]:
-            if key not in unlocked_boxes:
+            if key not in unlocked_boxes and key < n:
                 unlocked_boxes.append(key)
                 queue.append(key)
     return n == len(unlocked_boxes)
