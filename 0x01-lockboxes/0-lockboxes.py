c568c178017d586e7c1b84d83e4f3a5b1e598c36
nhamidn <hamid-savana@hotmail.fr>
2023-12-08 02:36:02 +0100

    edge case dfs

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,19 +3,23 @@
 
 
 def canUnlockAll(boxes):
-    """Function that check if all boxes can be opened using bfs algorithm."""
+    """Function that check if all boxes can be opened."""
     if (type(boxes)) is not list:
         return False
     n = len(boxes)
     if n == 0:
         return False
+    visited = [False] * n
+    visited[0] = True
 
-    queue = [0]
-    unlocked_boxes = [0]
-    while queue:
-        current_box = queue.pop(0)
-        for key in boxes[current_box]:
-            if key not in unlocked_boxes and key < n:
-                unlocked_boxes.append(key)
-                queue.append(key)
-    return n == len(unlocked_boxes)
+    def dfs(boxes, idx, visited):
+        """Function that implement the dfs algorithm."""
+        for key in boxes[idx]:
+            if not visited[key] and key < n:
+                visited[key] = True
+                if not all(visited):
+                    dfs(boxes, key, visited)
+
+    dfs(boxes, 0, visited)
+
+    return all(visited)
