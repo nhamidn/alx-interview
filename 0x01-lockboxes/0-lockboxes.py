7bc93e4519d8e12762a0538a02b6e6d047a4b339
nhamidn <hamid-savana@hotmail.fr>
2023-12-08 01:21:47 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,15 +7,13 @@ def canUnlockAll(boxes):
     n = len(boxes)
     if n == 0:
         return False
-    visited = [False] * n
-    visited[0] = True
-
-    stack = [0]
-    while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            if not visited[key]:
-                visited[key] = True
-                stack.append(key)
-
-    return all(visited)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
