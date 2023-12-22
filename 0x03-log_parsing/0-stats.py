6ba8cb24a6559f245302c5e213574d82824992ae
nhamidn <nhamid@student.1337.ma>
2023-12-22 22:34:38 +0100

    new code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,10 +3,33 @@
 import sys
 
 
+codes = {}
 count = 0
+total_files_size = 0
+
 for line in sys.stdin:
     count = count + 1
-    if count == 10:
-        print("File size: {}")
+    parts = line.split()
+
+    if len(parts) < 2:
+        continue
+
+    try:
+        status = int(parts[-2])
+        size = int(parts[-1])
+    except ValueError:
+        continue
+
+    total_files_size += size
+
+    if status in codes:
+        codes[status] += 1
+    else:
+        codes[status] = 1
+
+    if count % 10 == 0:
+        print(f"File size: {total_files_size}")
+        for code in sorted(codes.keys()):
+            print(f"{code}: {codes[code]}")
         pass
     pass
