0ca7a78ed7165a2ffae304a946ffa89794e523ae
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-12 04:29:54 +0000

    utf8 main

---
+++ 0x04-utf8_validation/0-main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+validUTF8 = __import__('0-validate_utf8').validUTF8
+
+data = [65]
+print(validUTF8(data))
+
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
+
+data = [229, 65, 127, 256]
+print(validUTF8(data))
+
+data = [0, 0, 0, 0, 0, 0]
+print(validUTF8(data))
+
+data = []
+print(validUTF8(data))
+
+data = [250, 145, 145, 145, 145]
+print(validUTF8(data))
+
+data = [345, 467]
+print(validUTF8(data))
+
+data = [235, 140]
+print(validUTF8(data))
+
+data = [240, 188, 128, 167]
+print(validUTF8(data))
+
+data = [467, 133, 108]
+print(validUTF8(data))
