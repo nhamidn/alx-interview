4533319048914ccdbe69851e53374a29a9ce24a6
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-22 18:42:33 +0100

    use dynamic programming

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,24 +7,15 @@ Rotate 2d matrix module.
 def rotate_2d_matrix(matrix):
     """Function that rotate 2d matrix by 90 degrees clockwise."""
     l_line = len(matrix[0])
-    l = l_line**2
-    m_test = [[-1, -1, -1],
-              [-1, -1, -1],
-              [-1, -1, -1]]
-    n = int((l) / 2)
-    for i in range(l):
+    len_m = l_line**2
+    dp = {}
+    n = int((len_m) / 2)
+    for i in range(len_m):
         x = int(i / l_line)
         y = int(i % l_line)
-        m_test[y][l_line-x-1] = matrix[x][y]
-        #m_test[x][y] = matrix[l_line-y-1][x]
-
-    for row in m_test:
-        print(row)
-
-if __name__ == "__main__":
-    matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
-
-    rotate_2d_matrix(matrix)
-    #print(matrix)
+        original_v = dp.get((x, y), None)
+        dp[(y, l_line-x-1)] = matrix[y][l_line-x-1]
+        if original_v is None:
+            matrix[y][l_line-x-1] = matrix[x][y]
+        else:
+            matrix[y][l_line-x-1] = original_v
